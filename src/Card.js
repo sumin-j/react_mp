ae9610559ae77fa35d65d157d198f4a08ad20bdb
sumin-j <soommmni@gmail.com>
2022-05-03 12:08:40 +0900

    컴포넌트에 style객체 연결

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,8 @@
 function Card(props){
-    console.log(props);
+    const style = {transform: `rotate(${360/props.num * props.index}deg) translateY(-150%)`}
+
     return(
-        <article>
+        <article style={style}>
                     <div className="inner">
                         <div className="pic">
                             <img src={`${props.path}/img/${props.data}.jpg`} />
--- src/Section.js
+++ src/Section.js
@@ -3,11 +3,12 @@ const path = process.env.PUBLIC_URL;
 
 function Section(){
     const arr=['Blizzards','Calm','Dusty_Road','Escape','Payday','Retreat','Seasonal','Vespers'];
+    const num = arr.length;
 
     return(
-        <section className="wrap">
+        <section className="wrap" style={{top: '120%'}}>
             {arr.map((data,idx)=>{
-                return <Card key={idx} data={data} path={path} />      
+                return <Card key={idx} data={data} path={path} num={num} index={idx} />      
             })}
         </section>
     )
