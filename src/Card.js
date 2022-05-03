42d620a2ebe0d997227324544026b04b00ea8e6c
sumin-j <soommmni@gmail.com>
2022-05-03 11:25:58 +0900

    부모컴포넌트에 자식컴포넌트로 props를 활용해 데이터 전달

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+function Card(props){
+    console.log(props);
+    return(
+        <article>
+                    <div className="inner">
+                        <div className="pic">
+                            <img src={`${props.path}/img/${props.data}.jpg`} />
+                        </div>
+                        <h2>{props.data}</h2> 
+                    </div>
+                </article>
+    )
+}
+
+export default Card;
+
--- src/Section.js
+++ src/Section.js
@@ -1,23 +1,27 @@
+import Card from "./Card";
 const path = process.env.PUBLIC_URL;
 
 function Section(){
     const arr=['Blizzards','Calm','Dusty_Road','Escape','Payday','Retreat','Seasonal','Vespers'];
 
-
-
-
     return(
         <section className="wrap">
             {arr.map((data,idx)=>{
-                return (
-                <article key={idx}>
-                    <p>{data}</p>
-                    <img src={`${path}/img/${data}.jpg`} />
-                </article>
-                )
+                // 자식인 Card 컴포넌트로 data,path정보값을 props로 전달
+                return <Card key={idx} data={data} path={path} />      
             })}
         </section>
     )
 }
 
 export default Section;
+
+/*
+    주석
+    연산되는 중괄호 안쪽에서는  일반 자시주석으로 활용가능하나
+    JSX 안쪽에서는 주석도 중괄호로 감싸줘야 됨
+
+    부모 컴포넌트에서 자식컴포넌트로 정보값을 전달할대 props를 활용
+    부모에서 자식으로 데이터를 전달가능
+    자식요소에서 props라는 객체로 해당 값을 전달 받음
+*/
