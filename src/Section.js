e252368d4a05a96f74d120481d002d521cca97ac
sumin-j <soommmni@gmail.com>
2022-05-03 10:48:55 +0900

    import방식으로 src폴더 내에서 이미지 불러오기

--- src/Section.js
+++ src/Section.js
@@ -1,5 +1,17 @@
+import pic1 from './img/Blizzards.jpg';
+import pic2 from './img/Calm.jpg';
+import pic3 from './img/Dusty_Road.jpg';
+import pic4 from './img/Escape.jpg';
+import pic5 from './img/Payday.jpg';
+import pic6 from './img/Retreat.jpg';
+import pic7 from './img/Seasonal.jpg';
+import pic8 from './img/Vespers.jpg';
+
+
+
 function Section(){
-    const arr=['Bizzards','Calm','Dusty_Road','Escape','Payday','Retreat','Seasonal','Vespoers'];
+    const imgs = [pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8]
+    const arr=['Blizzards','Calm','Dusty_Road','Escape','Payday','Retreat','Seasonal','Vespers'];
 
 /*
 - jsx문법 안쪽에서 반복처리할때는 map을 이용
@@ -9,9 +21,14 @@ function Section(){
 
 
     return(
-        <section className='wrap'>
+        <section className="wrap">
             {arr.map((data,idx)=>{
-                return <article key={idx}>{data}</article>
+                return (
+                <article key={idx}>
+                    <p>{data}</p>
+                    <img src={imgs[idx]} />
+                </article>
+                )
             })}
         </section>
     )
